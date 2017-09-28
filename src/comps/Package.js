5665203111728c15decda1675d8b515e8dae08ca
itsnhduc <nhduc.lhp@gmail.com>
2017-09-29 02:22:44 +0800

    Implement install cmd & readme injection

--- src/comps/Package.js
+++ src/comps/Package.js
@@ -1,23 +1,38 @@
 import React, { Component } from 'react'
-import { Row, Col } from 'react-materialize'
+import { Row, Col, CardPanel } from 'react-materialize'
+import clipboard from 'clipboard-js'
+import ReactMarkdown from 'react-markdown'
 import Jumbo from './Jumbo'
 
 class Package extends Component {
   constructor () {
     super()
     this.state = {
-      pkg: []
+      pkg: [],
+      readme: 'Loading README content...'
     }
   }
 
   componentDidMount () {
-    const fetchUrl = 'http://localhost:3000/api/spmpackages?filter[where][name]=' + this.props.match.params.name
-    fetch(fetchUrl)
+    const pkgInfoUrl = 'http://localhost:3000/api/spmpackages?filter[where][name]=' + this.props.match.params.name
+    fetch(pkgInfoUrl)
       .then(res => res.json())
       .then(pkgs => {
         this.setState({ pkg: pkgs[0] })
-        console.log(this.state.pkg)
       })
+      .then(() => {
+        const readmeInfoUrl = 'https://api.github.com/repos/' + this.state.pkg.author + '/' + this.state.pkg.name + '/readme'
+        fetch(readmeInfoUrl)
+          .then(res => res.json())
+          .then(readmeInfo => {
+            this.setState({ readme: atob(readmeInfo.content) })
+          })
+      })
+
+  }
+
+  copyCmd() {
+    clipboard.copy('spm install ' + this.state.pkg.name.replace('spm-pkg-', ''))
   }
 
   render () {
@@ -29,19 +44,35 @@ class Package extends Component {
         </Jumbo>
         <Row>
           <Col l={10} m={12} offset="l1 m0">
-            <section>
-              <h5>Version</h5>
-              <p>{this.state.pkg.version}</p>
-            </section>
-            <section>
-              <h5>Description</h5>
-              <p>{this.state.pkg.description}</p>
-            </section>
-            <section>
-              <h5>Stats</h5>
-              <p>Installs: {this.state.pkg.install_count}</p>
-              <p>Stars: {this.state.pkg.star_count}</p>
-            </section>
+            <Row>
+              <Col l={3} m={12}>
+                <CardPanel className="pkg-left-info">
+                  <section>
+                    <h5>Version</h5>
+                    <p>{this.state.pkg.version}</p>
+                  </section>
+                  <section>
+                    <h5>Standard Installation</h5>
+                    <pre>
+                      <code>
+                        $ spm install {this.state.pkg.name ? this.state.pkg.name.replace('spm-pkg-', '') : ''}
+                        <a className="copy-anchor" href="#" onClick={this.copyCmd.bind(this)}>Copy</a>
+                      </code>
+                    </pre>
+                  </section>
+                  <section>
+                    <h5>Description</h5>
+                    <p>{this.state.pkg.description}</p>
+                  </section>
+                </CardPanel>
+              </Col>
+              <Col l={9} m={12}>
+                <CardPanel>
+                  <ReactMarkdown source={this.state.readme}/>
+                </CardPanel>
+              </Col>
+            </Row>
+            
           </Col>
         </Row>
       </div>
