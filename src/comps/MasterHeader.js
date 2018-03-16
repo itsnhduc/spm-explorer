44449003b50e2b8e272232fca1dd6a2a16f06560
itsnhduc <nhduc.lhp@gmail.com>
2018-03-16 09:15:50 +0800

    Add dynamic nav display

--- src/comps/MasterHeader.js
+++ src/comps/MasterHeader.js
@@ -3,7 +3,23 @@ import { Navbar, NavItem } from 'react-materialize'
 import './MasterHeader.css'
 
 class MasterHeader extends Component {
-
+  constructor () {
+    super()
+    this.state = {
+      fixedNav: false
+    }
+  }
+  handleScroll (e) {
+    this.setState({
+      fixedNav: window.pageYOffset !== 0
+    })
+  }
+  componentDidMount () {
+    document.addEventListener('scroll', this.handleScroll.bind(this))
+  }
+  componentWillUnmount () {
+    document.removeEventListener('scroll', this.handleScroll.bind(this))
+  }
   render () {
     const items = [
       { key: 'SEARCH', href: '' },
@@ -13,7 +29,7 @@ class MasterHeader extends Component {
 
     return (
       <div className='MasterHeader'>
-        <Navbar brand='&nbsp;SPM Explorer' right>
+        <Navbar className={this.state.fixedNav ? 'fixed' : ''} brand='&nbsp;SPM Explorer' right>
           {items.map(i => {
             const isActive = window.location.pathname.split('/')[1] === i.href
             return (
