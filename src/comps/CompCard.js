7a6d7db37575e573c5f4c5cb8681dcf5a0538a37
itsnhduc <nhduc.lhp@gmail.com>
2018-03-18 09:19:46 +0800

    Add language badge

--- src/comps/CompCard.js
+++ src/comps/CompCard.js
@@ -4,8 +4,16 @@ import './CompCard.css'
 
 class CompCard extends Component {
   render () {
+    const title = this.props.card.name.replace('spm-pkg-', '') + '@' + this.props.card.commits[0].version
     return (
-      <Card title={<a href={'/package/' + this.props.card.name}><strong>{this.props.card.name.replace('spm-pkg-', '') + '@' + this.props.card.commits[0].version}</strong></a>}>
+      <Card title={
+        <span>
+          <a href={'/package/' + this.props.card.name}>
+            <strong>{title}</strong>
+          </a>
+          <span className='new badge' data-badge-caption=''>{this.props.card.language}</span>
+        </span>
+      }>
         <p>by <a href={'/user/' + this.props.card.author}><strong>{this.props.card.author}</strong></a></p>
         {this.props.card.description}
       </Card>
